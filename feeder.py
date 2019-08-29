33a373d7cf4233de3fabdd43c3227488833d9619
Tim Healy <healyt22@gmail.com>
2019-08-29 14:48:40 -0400

    import from s3 functionality completed

--- feeder.py
+++ feeder.py
@@ -10,19 +10,27 @@ class LoadSpreads():
         self.date_param = date_param
         if not date_param:
             self.date_param = datetime.today().strftime('%Y-%m-%d')
-    
+
+    def find_objs(self, prefix):
+        s3_objs = self.bucket.objects.filter(Prefix=prefix)
+        obj_keys = [obj.key for obj in s3_objs]
+        return(obj_keys[0])
+
     def get_spreads(self, obj_key):
-        content_object = self.s3.Object(self.bucket, obj_key)
-        content = content_object.get()['Body'].read().decode('utf-8')
+        content_object = self.s3.Object(self.bucket.name, obj_key)
+        content = content_object.get()['Body'].read()#.decode('utf-8')
         spreads_dict = json.loads(content)
         return(spreads_dict)
-    
+
     def main(self):
-        espn_obj_key = 'espn/spreads/{0}.json'.format(self.date_param)
+        espn_prefix = 'espn/spreads/{0}'.format(self.date_param)
+        espn_obj_key = self.find_objs(espn_prefix)
         espn_spreads = self.get_spreads(espn_obj_key)
-        os_obj_key = 'oddsshark/spreads/{0}.json'.format(self.date_param)
+
+        os_prefix = 'oddsshark/spreads/{0}'.format(self.date_param)
+        os_obj_key = self.find_objs(os_prefix)
         os_spreads = self.get_spreads(os_obj_key)
-        return(espn_spreads, os_spreads)
-        
+        print(espn_spreads, os_spreads)
+
 if __name__ == '__main__':
     LoadSpreads('atthletics').main()
