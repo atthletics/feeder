9adb17135c0c8c811c080061f165a0c48857bda7
healyt22 <healyt22@gmail.com>
2019-09-03 10:55:32 -0400

    added s3 obj loop

--- feeder.py
+++ feeder.py
@@ -19,11 +19,15 @@ class S3ToStage():
 
     def find_s3_objs(self):
         self.s3_objs = self.bucket.objects.filter(Prefix=self.prefix)
-        self.s3_obj_keys = [obj.key for obj in self.s3_objs]
-        return(self.s3_obj_keys)
+        s3_obj_keys = [obj.key for obj in self.s3_objs]
+        return(s3_obj_keys)
 
-    def get_data(self, obj_key):
+    def get_s3_data(self, s3_obj_key):
         content_object = self.s3.Object(self.bucket.name, obj_key)
         content = content_object.get()['Body'].read().decode('utf-8')
         spreads_dict = json.loads(content)
         return(spreads_dict)
+
+    def main(self):
+        s3_obj_keys = self.find_s3_objs()
+        self.s3_data = [self.get_s3_data(obj_key) for obj_key in s3_obj_keys]
