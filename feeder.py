dbb7a9af34bed63a412ffe29c03b9cb35f45e0d0
healyt22 <healyt22@gmail.com>
2019-09-03 10:44:49 -0400

    reorg

--- feeder.py
+++ feeder.py
@@ -4,20 +4,26 @@ import boto3
 import MySQLdb
 
 class S3ToStage():
-    def __init__(self, bucket, date_param=None):
+    def __init__(self, bucket, source, week_id, date_param=None):
         self.s3 = boto3.resource('s3')
         self.bucket = self.s3.Bucket(bucket)
-        self.date_param = date_param
         if not date_param:
-            self.date_param = datetime.today().strftime('%Y-%m-%d')
+            date_param = datetime.today().strftime('%Y-%m-%d')
+        self.params = {
+            'source'     : source,
+            'week_id'    : week_id,
+            'date_param' : date_param
+        }
+        prefix_path = 'data/{source}/week_id={week_id}/{date_param}'
+        self.prefix = prefix_path.format(**params)
 
     def find_obj_keys(self, prefix):
-        s3_objs = self.bucket.objects.filter(Prefix=prefix)
-        obj_keys = [obj.key for obj in s3_objs]
-        return(obj_keys)
+        self.s3_objs = self.bucket.objects.filter(Prefix=prefix)
+        self.s3_obj_keys = [obj.key for obj in self.s3_objs]
+        return(self.s3_obj_keys)
 
     def get_data(self, obj_key):
         content_object = self.s3.Object(self.bucket.name, obj_key)
-        content = content_object.get()['Body'].read()#.decode('utf-8')
+        content = content_object.get()['Body'].read().decode('utf-8')
         spreads_dict = json.loads(content)
         return(spreads_dict)
