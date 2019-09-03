3c369d04f84803a87c96774aed80b0a6076c61c3
healyt22 <healyt22@gmail.com>
2019-09-03 17:00:46 -0400

    added week id and scrape ts

--- feeder.py
+++ feeder.py
@@ -1,4 +1,4 @@
-import json
+import json, re
 from datetime import datetime
 import boto3
 import MySQLdb
@@ -16,18 +16,26 @@ class S3ToStage():
         }
         prefix_path = 'data/{source}/week_id={week_id}/{date_param}'
         self.prefix = prefix_path.format(**self.params)
+        self.s3_data = []
 
     def find_s3_objs(self):
         self.s3_objs = self.bucket.objects.filter(Prefix=self.prefix)
-        s3_obj_keys = [obj.key for obj in self.s3_objs]
-        return(s3_obj_keys)
+        self.s3_obj_keys = [obj.key for obj in self.s3_objs]
+        return(self.s3_obj_keys)
 
-    def get_s3_data(self, s3_obj_key):
+    def s3_data_feed(self, s3_obj_key):
         content_object = self.s3.Object(self.bucket.name, s3_obj_key)
         content = content_object.get()['Body'].read().decode('utf-8')
-        spreads_dict = json.loads(content)
-        return(spreads_dict)
+        data_dict = json.loads(content)
+        week_id = re.search('week_id=(.+?)/', s3_obj_key).group(1)
+        scrape_ts = re.search('/(.+?).json', s3_obj_key).group(1)
+        data_dict.update({
+            'week_id' : week_id,
+            'scrape_ts' : scrape_ts
+        })
+        return(data_dict)
 
     def main(self):
-        s3_obj_keys = self.find_s3_objs()
-        self.s3_data = [self.get_s3_data(obj_key) for obj_key in s3_obj_keys]
+        self.find_s3_objs()
+        for s3_obj_key in self.s3_obj_keys:
+            self.s3_data.append(s3_data_feed(s3_obj_key))
